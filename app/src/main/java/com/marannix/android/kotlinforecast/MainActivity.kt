202df86e70aef641ddaa3a55bf4f30708ab22a18
Joshua Onabanjo <joshua@memrise.com>
2019-05-11 21:03:11 +0100

    Initial commit and added dependencies

---
+++ app/src/main/java/com/marannix/android/kotlinforecast/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.marannix.android.kotlinforecast
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    // TODO: If anything goes wrong, go to
+    // https://www.youtube.com/watch?v=yDaaM3u389I
+    // video number 6:25
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
